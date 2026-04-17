739c06c95318ffdb6d646adbdd45281a33d3438e
Bob Strahan <strahanr@amazon.com>
2026-04-17 12:15:16 -0400

    feat(version): update all package versions to 0.5.7

--- lib/idp_cli_pkg/idp_cli/cli.py
+++ lib/idp_cli_pkg/idp_cli/cli.py
@@ -237,7 +237,7 @@ TEMPLATE_URLS = {
 
 
 @click.group()
-@click.version_option(version="0.5.6")
+@click.version_option(version="0.5.7")
 def cli():
     """
     IDP CLI - Batch document processing for IDP Accelerator
